e4709e33e0fe9c4c50960ee9eccf83c933a3bc98
BrittleFoot <i_o@rtural.ru>
2021-11-09 20:57:17 +0300

    ok, now db works

--- src/sudan/__init__.py
+++ src/sudan/__init__.py
@@ -1,8 +1,12 @@
 import logging
 
+from distutils.sysconfig import get_python_lib
+from pathlib import Path
+
 
 FORMAT = '[%(levelname)-5s][%(name)12s] %(message)s'
 logging.basicConfig(format=FORMAT)
+logger = logging.getLogger('sudan')
 
 
 
@@ -12,8 +16,25 @@ class Config:
     MIN_CONTIG_LEN = 200
     MAX_TRNA_LEN = 500
 
+    kingdom = 'Bacteria'
+    
+
+
+
+def get_db_path():
+    DB_NAME = 'sudan_db'
+    db_dir = Path(get_python_lib()) / DB_NAME
+    if not db_dir.is_dir():
+        db_dir = Path(__file__).parent / DB_NAME 
+
+    if not db_dir.is_dir():
+        logger.error(f'{db_dir} is not a database dir, something wrong!')
+
+    return db_dir
+       
 
 
 
 def main():
     print('Hello, World! This is sudan main function. It is perfectly empty.')
+    print(f'Although we found a database dir in {get_db_path()}')
